14f7b50b25994d603f32b9a0c1e96e1ad752323b
xxlostangle <xxlostangle@gmail.com>
2020-11-20 17:16:29 +0200

    working retun image with multiple plots

--- covid_site.py
+++ covid_site.py
@@ -5,101 +5,13 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from CovidSite.generate_plots import globalvars_class as data_maker
+
 
 
 app = Flask(__name__)
 
-class globalvars_class:
-    def __init__(self):
-        self.moving_avarge=7
-        self.covid_df = self.covid_pd_make()
-        self.covid_positive = self.covid_positive_frame()
-        self.database_date = self.updated_to()
-        self.daily_covid_positive = self.covid_positive.groupby(self.covid_positive.index).sum()
-    #functions
-    def covid_pd_make(self):
-        csv_lab = pd.read_csv("corona_lab_tests_ver_0090.csv")
-        csv_lab.index = pd.DatetimeIndex(csv_lab["result_date"])
-        csv_lab.drop(["result_date"], axis=1, inplace=True)
-        csv_lab["corona_result"][csv_lab["corona_result"] == "שלילי"] = 0
-        csv_lab["corona_result"][csv_lab["corona_result"] == "חיובי"] = 1
-        csv_lab["is_first_Test"][csv_lab["is_first_Test"] == "Yes"] = 1
-        csv_lab["is_first_Test"][csv_lab["is_first_Test"] == "No"] = 0
-        return csv_lab
-    def covid_positive_frame(self):
-        temp = self.covid_df[((self.covid_df["corona_result"] == 0) | (self.covid_df["corona_result"] == 1))][
-            "corona_result"].to_frame()
-        return temp
-    def updated_to(self):
-        return self.covid_df.index.max().strftime("%d-%m-%y")
-
-globalvars = globalvars_class()
-
-def plot_to_img(fig):
-    output = io.BytesIO()
-    FigureCanvasAgg(fig).print_png(output)
-    return Response(output.getvalue(), mimetype="image/png")
-
-def interact_positive_ema(ma):
-    fig = plt.figure(figsize=(16, 10))
-    covid_positive = globalvars.covid_positive
-    covid_positive = covid_positive.groupby(covid_positive.index).sum()
-    covid_positive["corona_result"].plot(label="Corona positive results per day", figsize=(16, 10))
-    covid_positive["corona_result"].rolling(window=ma).mean().plot(alpha=0.8, label=str(ma) + "MA")
-    plt.legend()
-    return plot_to_img(fig)
-
-
-def res_plot_3ma():
-    fig = plt.figure(figsize=(16, 10))
-    daily_covid_positive = globalvars.covid_positive.groupby(globalvars.covid_positive.index).sum()
-    daily_covid_positive["corona_result"].plot(label="Corona positive results")
-    daily_covid_positive["corona_result"].rolling(window=7).mean().plot(alpha=0.8, label="7MA")
-    daily_covid_positive["corona_result"].rolling(window=30).mean().plot(alpha=0.8, label="30MA")
-    daily_covid_positive["corona_result"].rolling(window=60).mean().plot(alpha=0.8, label="60MA")
-    plt.legend()
-    return plot_to_img(fig)
-
-def res_first_test_and_pos_ema():
-
-    fig = plt.figure(figsize=(16, 10))
-    first_test = globalvars.covid_df[globalvars.covid_df["is_first_Test"] == 1]
-    another_test = globalvars.covid_df[globalvars.covid_df["is_first_Test"] == 0]
-
-
-    first_test.drop(["test_date"], axis=1, inplace=True)
-    another_test.drop(["test_date"], axis=1, inplace=True)
-
-    first_test["is_first_Test"].groupby(first_test.index).size().plot(label="First test")
-
-    another_test["is_first_Test"].groupby(another_test.index).size().plot(label="Not First test")
-
-    globalvars.daily_covid_positive["corona_result"].plot(label="positive").plot()
-    globalvars.daily_covid_positive["corona_result"].rolling(window=7).mean().plot(alpha=0.8, label="7MA")
-
-
-    plt.legend()
-    return plot_to_img(fig)
-
-def res_first_test_and_pos_clean():
-    fig = plt.figure(figsize=(16, 10))
-    first_test = globalvars.covid_df[(globalvars.covid_df["is_first_Test"] == 1) & (globalvars.covid_df["corona_result"] == 1)]
-    other_test = globalvars.covid_df[(globalvars.covid_df["is_first_Test"] == 0) & (globalvars.covid_df["corona_result"] == 1)]
-    first_test["corona_result"].groupby(first_test.index).sum().plot(label="first test")
-    other_test["corona_result"].groupby(other_test.index).sum().plot(label="Not first test")
-    globalvars.covid_positive["corona_result"].plot(label="positive").plot()
-    plt.legend()
-    return plot_to_img(fig)
-
-def res_quantire():
-    fig = plt.figure(figsize=(16, 10))
-    globalvars.covid_positive["7MA"] = globalvars.covid_positive.rolling(window=7).mean()
-    globalvars.covid_positive.loc["2020-03-14":"2020-04-30"].plot()
-    globalvars.covid_positive.loc["2020-09-18":"2020-10-13"].plot()
-    plt.legend()
-    return plot_to_img(fig)
-
-
+plot_maker = data_maker()
 
 @app.route("/")
 def index():
@@ -108,17 +20,21 @@ def index():
 @app.route("/interact_ma")
 def interact_page():
     try:
-        globalvars.moving_avarge = int(request.args.get("ma", 7))
+        plot_maker.moving_avarge = int(request.args.get("ma", 7))
     except:
-        globalvars.moving_avarge = 7
-    return render_template("emainteractive.html", database_date=globalvars.database_date, ma=globalvars.moving_avarge)
+        plot_maker.moving_avarge = 7
+    return render_template("emainteractive.html", database_date=plot_maker.database_date, ma=plot_maker.moving_avarge)
 
 
 #Responsive ma graph
 @app.route("/inter_plot.png")
 def interact_plot():
-    return interact_positive_ema(ma=globalvars.moving_avarge)
+    return plot_maker.interact_positive_ema(ma=plot_maker.moving_avarge)
 
+
+@app.route("/inter_plot_saved.png")
+def interact_plot():
+    return plot_maker.interact_positive_ema(ma=plot_maker.moving_avarge)
 
 
 
@@ -132,24 +48,25 @@ def research():
 #plotting 3 different ma to a picture
 @app.route("/plot_3_ma.png")
 def mul_ema_plot():
-    return res_plot_3ma()
+    return plot_maker.res_plot_3ma()
 
 
 #plotting positive and first and not first test plot with ma
 @app.route("/positive_first_ma.png")
 def first_test_pos_ma():
-    return res_first_test_and_pos_ema()
+    return plot_maker.res_first_test_and_pos_ema()
 
 #plotting positive and first and not first test plot
 
 @app.route("/positive_first.png")
 def first_test_pos():
-    return res_first_test_and_pos_clean()
+    return plot_maker.res_first_test_and_pos_clean()
+
 
 
 @app.route("/quantire.png")
 def quantire_plot():
-    return res_quantire()
+    return plot_maker.res_quantire()
 
 
 if __name__ == "__main__":
---
+++ generate_plots.py
@@ -0,0 +1,111 @@
+import io
+from flask import Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from pathlib import Path
+print('Running' if __name__ == '__main__' else 'Importing', Path(__file__).resolve())
+
+
+class globalvars_class:
+    def __init__(self):
+        self.moving_avarge=7
+        self.covid_df = self.covid_pd_make()
+        self.covid_positive = self.covid_positive_frame()
+        self.database_date = self.updated_to()
+        self.daily_covid_positive = self.covid_positive.groupby(self.covid_positive.index).sum()
+        self.fig = plt.figure(figsize=(16, 10))
+        self.fig_saved = plt.figure(figsize=(16, 10))
+    #functions
+    def covid_pd_make(self):
+        csv_lab = pd.read_csv("corona_lab_tests_ver_0090.csv")
+        csv_lab.index = pd.DatetimeIndex(csv_lab["result_date"])
+        csv_lab.drop(["result_date"], axis=1, inplace=True)
+        csv_lab["corona_result"][csv_lab["corona_result"] == "שלילי"] = 0
+        csv_lab["corona_result"][csv_lab["corona_result"] == "חיובי"] = 1
+        csv_lab["is_first_Test"][csv_lab["is_first_Test"] == "Yes"] = 1
+        csv_lab["is_first_Test"][csv_lab["is_first_Test"] == "No"] = 0
+        return csv_lab
+
+    def covid_positive_frame(self):
+        temp = self.covid_df[((self.covid_df["corona_result"] == 0) | (self.covid_df["corona_result"] == 1))][
+            "corona_result"].to_frame()
+        return temp
+    def updated_to(self):
+        return self.covid_df.index.max().strftime("%d-%m-%y")
+
+    def plot_to_img(self):
+        output = io.BytesIO()
+        FigureCanvasAgg(self.fig).print_png(output)
+        self.fig.clf()
+        return Response(output.getvalue(), mimetype="image/png")
+    def plot_to_img_saved(self):
+        output = io.BytesIO()
+        FigureCanvasAgg(self.fig_saved).print_png(output)
+        return Response(output.getvalue(), mimetype="image/png")
+
+    def interact_positive_ema(self,ma):
+        covid_positive = self.covid_positive
+        covid_positive = covid_positive.groupby(covid_positive.index).sum()
+        covid_positive["corona_result"].plot(label="Corona positive results per day", figsize=(16, 10))
+        covid_positive["corona_result"].rolling(window=ma).mean().plot(alpha=0.8, label=str(ma) + "MA")
+        plt.legend()
+        return self.plot_to_img()
+
+    def interact_positive_ma_saved(self,ma):
+        covid_positive = self.covid_positive
+        covid_positive = covid_positive.groupby(covid_positive.index).sum()
+        covid_positive["corona_result"].plot(label="Corona positive results per day", figsize=(16, 10))
+        covid_positive["corona_result"].rolling(window=ma).mean().plot(alpha=0.8, label=str(ma) + "MA")
+        plt.legend()
+        return self.plot_to_img()
+
+
+    def res_plot_3ma(self):
+        daily_covid_positive = self.covid_positive.groupby(self.covid_positive.index).sum()
+        daily_covid_positive["corona_result"].plot(label="Corona positive results")
+        daily_covid_positive["corona_result"].rolling(window=7).mean().plot(alpha=0.8, label="7MA")
+        daily_covid_positive["corona_result"].rolling(window=30).mean().plot(alpha=0.8, label="30MA")
+        daily_covid_positive["corona_result"].rolling(window=60).mean().plot(alpha=0.8, label="60MA")
+        plt.legend()
+        return self.plot_to_img()
+
+    def res_first_test_and_pos_ema(self):
+        first_test = self.covid_df[self.covid_df["is_first_Test"] == 1]
+        another_test = self.covid_df[self.covid_df["is_first_Test"] == 0]
+
+        first_test.drop(["test_date"], axis=1, inplace=True)
+        another_test.drop(["test_date"], axis=1, inplace=True)
+
+        first_test["is_first_Test"].groupby(first_test.index).size().plot(label="First test")
+
+        another_test["is_first_Test"].groupby(another_test.index).size().plot(label="Not First test")
+
+        self.daily_covid_positive["corona_result"].plot(label="positive").plot()
+        self.daily_covid_positive["corona_result"].rolling(window=7).mean().plot(alpha=0.8, label="7MA")
+
+        plt.legend()
+        return self.plot_to_img()
+
+    def res_first_test_and_pos_clean(self):
+        fig = plt.figure(figsize=(16, 10))
+        first_test = self.covid_df[
+            (self.covid_df["is_first_Test"] == 1) & (self.covid_df["corona_result"] == 1)]
+        other_test = self.covid_df[
+            (self.covid_df["is_first_Test"] == 0) & (self.covid_df["corona_result"] == 1)]
+        first_test["corona_result"].groupby(first_test.index).sum().plot(label="first test")
+        other_test["corona_result"].groupby(other_test.index).sum().plot(label="Not first test")
+        self.covid_positive["corona_result"].plot(label="positive").plot()
+        plt.legend()
+        return self.plot_to_img()
+
+    def res_quantire(self):
+        fig = plt.figure(figsize=(16, 10))
+        self.covid_positive["7MA"] = self.covid_positive.rolling(window=7).mean()
+        self.covid_positive.loc["2020-03-14":"2020-04-30"].plot()
+        self.covid_positive.loc["2020-09-18":"2020-10-13"].plot()
+        plt.legend()
+        return self.plot_to_img()
+
+
